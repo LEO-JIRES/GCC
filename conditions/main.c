c1380d1b9ee59d9329eb1eddaf91fdd15191d994
LEO-JIRES <leokenhoung@gmail.com>
2025-06-03 00:26:48 +0000

    exo: conditions

---
+++ conditions/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void guessNumber(int number) {
+        // TODO: write your code here
+}
+
+int main() {
+        guessNumber(500);
+            guessNumber(600);
+                guessNumber(555);
+}
